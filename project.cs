4b3266fa62408af44ad0d40185b491fd71c97fc1
Tuna <58302747+TunaUnderwoods@users.noreply.github.com>
2021-06-10 04:32:00 +0300

    Update project.cs

--- project.cs
+++ project.cs
@@ -1,1 +1,31 @@
+public class Tutorial
+{
+	public static void Main(string[] args)
+		=> new Program().MainAsync().GetAwaiter().GetResult();
+
+	public async Task MainAsync()
+	{
+	}
+  
+  private Task Log(LogMessage msg)
+{
+	Console.WriteLine(msg.ToString());
+	return Task.CompletedTask;
+}
+private DiscordSocketClient _client;
+
+public async Task MainAsync()
+{
+    _client = new DiscordSocketClient();
+
+    var token = "token";
+
+    _client.Log += Log;
+    await _client.LoginAsync(TokenType.Bot, token);
+    await _client.StartAsync();
+
+    await Task.Delay(-1);
+}
+  
+}
 
